469aa458db07b7fb0865a537c6c61374fbc153b2
glegorretar <61630769+glegorretar@users.noreply.github.com>
2020-03-26 13:22:49 -0600

    Add files via upload

---
+++ main_u3a1.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+
+void main() {
+  runApp(MaterialApp(
+    home: MyButton(),
+  ));
+}
+
+class MyButton extends StatefulWidget {
+  @override
+  MyButtonState createState() => MyButtonState();
+}
+
+class MyButtonState extends State<MyButton> {
+  int counter = 0;
+  List<String> strings = ['Programming', 'Dart', 'is', "great!!"];
+  String displayedString = "Excelent day!";
+
+  void onPressOfButton() {
+    setState(() {
+      displayedString = strings[counter];
+      counter = counter < 3 ? counter + 1 : 0;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Stateful Widget"),
+        backgroundColor: Colors.deepOrange,
+      ),
+      body: Container(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(displayedString, style: TextStyle(fontSize: 40.0)),
+              Padding(padding: EdgeInsets.all(10.0)),
+              RaisedButton(
+                child: Text(
+                  "Press me",
+                  style: TextStyle(color: Colors.white),
+                ),
+                color: Colors.blue,
+                onPressed: onPressOfButton,
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
